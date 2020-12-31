d552439bf6ba5d76b599e558788b5f087282a38f
andormade <hngrhorace@gmail.com>
2020-12-31 19:27:03 +0000

    feat: support converting more than 1000 objects

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 require('dotenv').config();
-const fetch = require('node-fetch');
 const fs = require('fs').promises;
 const path = require('path');
 const { exec } = require('child_process');
 const AWS = require('aws-sdk');
 
-const ORIGINALS_EXT = '.jpg';
-
-async function getOriginals(s3, extension) {
+async function listObjectsPromise(s3, marker) {
 	return new Promise((resolve, reject) => {
-		s3.listObjects({ Bucket: process.env.SOURCE_BUCKET }, function (err, data) {
+		s3.listObjects({ Bucket: process.env.SOURCE_BUCKET, MaxKeys: 5, Marker: marker }, async (err, data) => {
 			if (err) {
 				return reject(err);
 			}
-			resolve(
-				data['Contents']
-					.map(({ Key, LastModified }) => ({
-						key: Key,
-						lastModified: LastModified,
-					}))
-					.filter(({ key }) => key.endsWith(extension))
-			);
+
+			const originals = data['Contents']
+				.map(({ Key, LastModified }) => ({
+					key: Key,
+					lastModified: LastModified,
+				}))
+				.filter(({ key }) => key.endsWith('.jpg'));
+
+			resolve(originals);
 		});
 	});
+}
+
+async function getOriginals(s3, callback) {
+	let last;
+	while (true) {
+		const originals = await listObjectsPromise(s3, last);
+
+		if (originals.length === 0) {
+			break;
+		}
+
+		last = originals[originals.length - 1].key;
+
+		for (let i = 0; i < originals.length; i++) {
+			await callback(originals[i]);
+		}
+	}
 }
 
 async function downloadOriginal(s3, key) {
@@ -35,7 +50,6 @@ async function downloadOriginal(s3, key) {
 
 			await fs.mkdir(`.cache/originals/${path.dirname(key)}`, { recursive: true });
 			await fs.writeFile(`.cache/originals/${key}`, data.Body);
-			console.log(key, 'done');
 			resolve();
 		});
 	});
@@ -58,7 +72,6 @@ async function uploadOptimized(s3, key, ContentType = 'image/jpg') {
 					console.log(err);
 					return reject(err);
 				}
-				console.log(key, 'done');
 				resolve(data);
 			}
 		);
@@ -82,7 +95,6 @@ async function optimize(key) {
 					console.log(`${stdout}`);
 				}
 				resolve();
-				console.log(key, 'done');
 			}
 		);
 	});
@@ -96,23 +108,15 @@ async function optimize(key) {
 		secretAccessKey: process.env.SECRET_KEY,
 	});
 
-	const originals = await getOriginals(s3, ORIGINALS_EXT);
-
-	console.log('Downloading originals...');
-
-	for (let i = 0; i < originals.length; i++) {
-		await downloadOriginal(s3, originals[i].key);
-	}
-
-	console.log('Optimizing originals...');
-
-	for (let i = 0; i < originals.length; i++) {
-		await optimize(originals[i].key);
-	}
-
-	console.log('Uploading optimized images...');
-
-	for (let i = 0; i < originals.length; i++) {
-		await uploadOptimized(s3, originals[i].key);
-	}
+	await getOriginals(s3, async ({ key }) => {
+		process.stdout.write('Downloading: ' + key);
+		await downloadOriginal(s3, key);
+		process.stdout.write(' \x1b[32mdone\n\x1b[0m');
+		process.stdout.write('Optimizing: ' + key);
+		await optimize(key);
+		process.stdout.write(' \x1b[32mdone\n\x1b[0m');
+		process.stdout.write('Uploading: ' + key);
+		await uploadOptimized(s3, key);
+		process.stdout.write(' \x1b[32mdone\n\x1b[0m');
+	});
 })();
